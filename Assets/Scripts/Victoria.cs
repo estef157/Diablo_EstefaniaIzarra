452571d7b92be1dd4151e02f38bc17bb8d53310d
estef <estefaniaizarra8@gmail.com>
2025-02-17 10:02:33 +0100

    1702

---
+++ Assets/Scripts/Victoria.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victoria : MonoBehaviour
+{
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+  
+}
